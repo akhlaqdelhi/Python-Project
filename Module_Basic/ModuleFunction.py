fc10fd1f6d2ecb48439c8082ac9e10f2b686d1e5
Akhlaq Ahmed <akhlaqdelhi@live.in>
2023-07-15 18:39:47 +0530

    gitignore file addded

--- Module_Basic/ModuleFunction.py
+++ Module_Basic/ModuleFunction.py
@@ -2,13 +2,16 @@ import random
 
 print(random.randint(2, 17))
 
-
+# =============================================================
 
 def multiply(num1, num2):
+    #  a sample function that take 2 arguments an return a value
     return num1*num2
 
 
 n1 = input("Enter first number:")
 n2 = input("Enter second number:")
 result = multiply(int(n1), int(n2))
-print(result)
+print("the result value is= "+str(result))
+# print(result)
+# =============================================================
